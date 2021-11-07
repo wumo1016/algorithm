a54e606b6f04478750dc1260c3f58dce1de83c5f
wumo1016 <760478279@qq.com>
2021-11-07 15:50:41 +0800

    chore(83)

---
+++ leetcode/83.js
@@ -0,0 +1,51 @@
+function deleteDuplicates(head) {
+  let cur = head
+  while (cur) {
+    let next = cur.next
+    if (next && cur.val === next.val) {
+      cur.next = next.next
+    } else {
+      cur = cur.next
+    }
+  }
+  return head
+}
+
+const data1 = {
+  val: 1,
+  next: {
+    val: 1,
+    next: {
+      val: 2,
+      next: {
+        val: 3
+      }
+    }
+  }
+}
+
+const data2 = {
+  val: 1,
+  next: {
+    val: 1,
+    next: {
+      val: 1
+    }
+  }
+}
+
+console.log(deleteDuplicates(data1))
+console.log(deleteDuplicates(data2))
+
+/* // 官方题解
+function deleteDuplicates(head) {
+  let cur = head
+  while (cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next
+    } else {
+      cur = cur.next
+    }
+  }
+  return head
+} */
