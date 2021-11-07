49906c3273b589453ec094066b05f5ef978d00d1
wumo1016 <760478279@qq.com>
2021-11-07 16:01:25 +0800

    chore(83)

--- leetcode/83.js
+++ leetcode/83.js
@@ -1,5 +1,11 @@
-/* 删除排序链表中重复元素 */
+/* 删除排序链表中重复元素
+- 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素只出现一次，返回同样按升序排列的结果链表。
+*/
 
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 function deleteDuplicates(head) {
   let cur = head
   while (cur) {
