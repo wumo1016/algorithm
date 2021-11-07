089fc1b143173a3ae203c2128b44c448d521a5da
wumo1016 <760478279@qq.com>
2021-11-07 17:34:34 +0800

    chore(链表)

--- doc/链表/index.js
+++ doc/链表/index.js
@@ -3,3 +3,16 @@ function ListNode(val, next) {
   this.val = val === undefined ? 0 : val
   this.next = next === undefined ? null : next
 }
+
+// 链表反转
+function reverse(head) {
+  let prev = null
+  let cur = head
+  while (cur) {
+    const next = cur.next
+    cur.next = prev
+    prev = cur
+    cur = next
+  }
+  return prev
+}
