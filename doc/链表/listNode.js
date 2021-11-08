c9cbd5f64bfb79329ed539fd7ec14ce992ee3e1e
wumo1016 <760478279@qq.com>
2021-11-08 21:35:58 +0800

    chore(链表)

---
+++ doc/链表/listNode.js
@@ -0,0 +1,55 @@
+// 节点 + 指针
+class Node {
+  constructor(val, next = null) {
+    this.val = val
+    this.next = next
+  }
+}
+
+function ListNode() {
+  let head = new Node(1)
+  head.next = new Node(2)
+  head.next.next = new Node(3)
+  console.log(head)
+
+  let p = head,
+    ret = ''
+  while (p) {
+    ret += `${p.val} => `
+    p = p.next
+  }
+  ret += 'null'
+  console.log(ret)
+}
+ListNode()
+
+// 双数组
+function DoubleArray() {
+  const data = [] // 数据
+  const next = [] // 指针
+
+  function addNode(index, p, value) {
+    next[p] = next[index]
+    next[index] = p
+    data[p] = value
+  }
+
+  let head = 3
+  data[3] = 'a'
+
+  addNode(3, 5, 'b')
+  addNode(5, 7, 'c')
+  addNode(7, 2, 'd')
+  addNode(2, 1, 'e')
+  addNode(7, 4, 'f')
+
+  let p = head,
+    ret = ''
+  while (p) {
+    ret += `${data[p]} => `
+    p = next[p]
+  }
+  ret += null
+  console.log(ret)
+}
+DoubleArray()
