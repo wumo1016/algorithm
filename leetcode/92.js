da5405d31f7fe7c4e2cfd12ec42a8667409037f8
wumo1016 <760478279@qq.com>
2021-11-08 17:52:07 +0800

    chore(92)

--- leetcode/92.js
+++ leetcode/92.js
@@ -1,7 +1,8 @@
 /* 反转链表
-- 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回反转后的链表 (left和right交换位置)
+- 给你单链表的头指针 head 和两个整数 left 和 right 
+- 其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点
+- 返回反转后的链表
 */
-
 const data1 = {
   val: 1,
   next: {
@@ -24,7 +25,75 @@ const data1 = {
  * @param {number} right
  * @return {ListNode}
  */
-function reverseBetween(head, left, right) {}
+function reverseBetween(head, left, right) {
+  if (!head) return head
+  if (left === right) return head
+  let cacheHead = JSON.parse(JSON.stringify(head)),
+    oldTail,
+    dealHead
 
-reverseBetween(data1, 1, 2)
-// console.log(JSON.stringify(reverseBetween(data1, 1, 2)))
+  const reverseList = head => {
+    let prev = null
+    let cur = head
+    while (cur) {
+      const next = cur.next
+      cur.next = prev
+      prev = cur
+      cur = next
+    }
+    return prev
+  }
+
+  let index = 1
+  let cur = head
+  while (cur) {
+    if (index === left) {
+      dealHead = cur
+    }
+    if (index === right) {
+      oldTail = cur.next
+      cur.next = null
+    }
+    index++
+    cur = cur.next
+  }
+
+  let newHead = reverseList(dealHead)
+
+  // 先拼接尾 将尾拼接到反转后的后边
+  if (oldTail) {
+    let cur = newHead
+    while (cur) {
+      const next = cur.next
+      if (!next) {
+        cur.next = oldTail
+        break
+      }
+      cur = next
+    }
+  }
+
+  // 拼接头
+  if (left > 1) {
+    let index = 1
+    let cur = cacheHead
+    while (cur) {
+      const next = cur.next
+      if (index + 1 === left) {
+        cur.next = newHead
+        newHead = cacheHead
+        break
+      }
+      cur = next
+      index++
+    }
+  }
+
+  return newHead
+}
+
+console.log(str(reverseBetween(data1, 4, 5)))
+
+function str(s) {
+  return JSON.stringify(s)
+}
