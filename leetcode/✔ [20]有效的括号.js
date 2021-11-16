95c0edd25364495441d626359e4462822e6be4a0
wumo1016 <760478279@qq.com>
2021-11-16 20:08:32 +0800

    feat(20)

---
+++ leetcode/✔ [20]有效的括号.js
@@ -0,0 +1,33 @@
+/* 有效的括号
+- 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+- 左括号必须用相同类型的右括号闭合
+- 左括号必须以正确的顺序闭合
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(s) {
+  const list = [],
+    len = s.length,
+    map = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+    }
+
+  for (let i = 0; i < len; i++) {
+    const v = s[i]
+    if (!map[v]) {
+      list.push(v)
+    } else if (list.pop() !== map[v]) {
+      return false
+    }
+  }
+  return list.length === 0
+}
+
+const data = '({})[[]]((({}))){{[[]]()()}}'
+
+console.log(isValid(data))
