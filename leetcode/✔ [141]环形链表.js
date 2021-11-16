62f35a89bee384b725d368b42f915271ce7e6210
wumo1016 <760478279@qq.com>
2021-11-16 16:09:54 +0800

    chore(141)

--- leetcode/✔ [141]环形链表.js
+++ leetcode/✔ [141]环形链表.js
@@ -21,20 +21,6 @@ function hasCycle(head) {
   return false
 }
 
-// 快慢指针
-function hasCycle1(head) {
-  let slow = head
-  let fast = head
-  while (fast && fast.next) {
-    slow = slow.next
-    fast = fast.next.next
-    if (slow === fast) {
-      return true
-    }
-  }
-  return false
-}
-
 class ListNode {
   constructor(val, next) {
     this.val = val
@@ -57,6 +43,19 @@ console.log(hasCycle1(a))
 console.log(hasCycle2(a))
 console.log(hasCycle3(a))
 
+// 快慢指针
+function hasCycle1(head) {
+  let slow = head
+  let fast = head
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) {
+      return true
+    }
+  }
+  return false
+}
 // 利用JSON.stringfy
 function hasCycle2(head) {
   try {
