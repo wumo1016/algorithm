e537d615345701013d3db1d70af6de50ded3d979
wumo1016 <760478279@qq.com>
2021-11-22 17:43:09 +0800

    chore(859)

--- leetcode/[859]亲密字符串.js
+++ leetcode/[859]亲密字符串.js
@@ -43,3 +43,35 @@ console.log(buddyStrings('ab', 'ba')) // true
 console.log(buddyStrings('ab', 'ab')) // false
 console.log(buddyStrings('aa', 'aa')) // true
 console.log(buddyStrings('aaaaaaabc', 'aaaaaaacb')) // true
+
+// 官方优化版
+function buddyStrings1() {
+  if (s.length != goal.length) {
+    return false
+  }
+
+  if (s === goal) {
+    const count = new Array(26).fill(0)
+    for (let i = 0; i < s.length; i++) {
+      count[s[i].charCodeAt() - 'a'.charCodeAt()]++
+      if (count[s[i].charCodeAt() - 'a'.charCodeAt()] > 1) {
+        return true
+      }
+    }
+    return false
+  } else {
+    let first = -1,
+      second = -1
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] !== goal[i]) {
+        if (first === -1) first = i
+        else if (second === -1) second = i
+        else return false
+      }
+    }
+
+    return (
+      second !== -1 && s[first] === goal[second] && s[second] === goal[first]
+    )
+  }
+}
