b464644aca0b21317fca80d93b8a323dba3e5f50
wumo1016 <760478279@qq.com>
2021-11-24 14:48:08 +0800

    chore(21)

--- leetcode/✔ [21]合并两个有序链表.js
+++ leetcode/✔ [21]合并两个有序链表.js
@@ -45,9 +45,27 @@ const data2 = {
   next: {
     val: 3,
     next: {
-      val: 4
+      val: 4,
+      next: {
+        val: 5 
+      }
     }
   }
 }
 
 console.log(JSON.stringify(mergeTwoLists(data1, data2)))
+
+// 递归
+function mergeTwoLists1(l1, l2) {
+  if (l1 === null) {
+    return l2
+  } else if (l2 === null) {
+    return l1
+  } else if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
+  }
+}
