7433aeb885346aa3c3f7ca2a50028135d26575c8
wumo1016 <760478279@qq.com>
2021-11-30 15:58:21 +0800

    chore(328)

--- leetcode/[328]奇偶链表.js
+++ leetcode/[328]奇偶链表.js
@@ -48,20 +48,52 @@ const data = {
 // console.log(JSON.stringify(oddEvenList(data)))
 console.log(JSON.stringify(oddEvenList1(data)))
 
+// 优化版 只遍历奇数节点
 function oddEvenList1(head) {
   if (!head || !head.next || !head.next.next) return head
   let [odd, acc, fast] = [head, head.next, head.next.next]
   while (fast) {
     const next = fast.next
-
+    // 交换位置
     fast.next = odd.next // 3 => 2
     odd.next = fast // 1 => 3
     acc.next = next // 2 => 4
-
+    // 奇偶指针后移
     odd = odd.next
     acc = next
-
+    // 遍历节点后移
     fast = next && next.next
   }
   return head
+}
+
+/* 
+奇数指针head从链头向尾每次移动两格。p暂存两格后的第3格
+head.next的next指向p.next。head的next指向p。head = p移动两格
+迭代后，head指向奇数位最后节点，已不是最初head
+需n把偶数位第一节点，最初head.next暂存。与head相连
+需m把奇数位第一节点，最初head暂存。返回
+*/
+var oddEvenList = function (head, m = head) {
+  if (head) {
+    var n = head.next
+    while (head.next && head.next.next) {
+      var p = head.next.next
+      head.next.next = p.next
+      head = head.next = p
+    }
+    head.next = n
+  }
+  return m
+}
+
+
+var oddEvenList = function (head, n = head ? head.next : null, p) {
+  return (
+    head &&
+      (head.next && (p = head.next.next)
+        ? ((head.next.next = p.next), oddEvenList((head.next = p), n))
+        : (head.next = n)),
+    head
+  )
 }
