a9363ea980d1507220ff35be76b3f11486223dde
wumo1016 <760478279@qq.com>
2021-12-04 17:39:22 +0800

    chore(844)

--- leetcode/[844]比较含退格的字符串.js
+++ leetcode/[844]比较含退格的字符串.js
@@ -1,5 +1,5 @@
 /* 比较含退格的字符串(https://leetcode-cn.com/problems/backspace-string-compare/)
-- 给定s和t两个字符串，当它们分别被输入到空白的文本编辑器后，请你判断二者是否相等
+- 给定s和t两个字符串，#代表退格操作 就是删除前面一个字符
 - 如果相等，返回true 否则，返回false
 - 如果对空文本输入退格字符，文本继续为空
 */
@@ -9,4 +9,21 @@
  * @param {string} t
  * @return {boolean}
  */
-function backspaceCompare(s, t) {}
+function backspaceCompare(s, t) {
+  const deal = str => {
+    const [stack, len] = [[], str.length]
+    for (let i = 0; i < len; i++) {
+      const val = str[i]
+      if (val === '#') {
+        stack.pop()
+      } else {
+        stack.push(val)
+      }
+    }
+    return stack.toString()
+  }
+  return deal(s) === deal(t)
+}
+
+console.log(backspaceCompare('ab#c', 'ad#c'))
+console.log(backspaceCompare('a#c', 'ac'))
