825e2e569fe41ec18ac642365c9018c9f7e034ab
wumo1016 <760478279@qq.com>
2021-12-05 10:07:46 +0800

    chore(1249)

--- leetcode/[1249]移除无效的括号.js
+++ leetcode/[1249]移除无效的括号.js
@@ -6,7 +6,7 @@
  * @param {string} s
  * @return {string}
  */
-function minRemoveToMakeValid(s) {
+function minRemoveToMakeValid1(s) {
   let [stack, len, res] = [[], s.length, []]
   for (let i = 0; i < len; i++) {
     const val = s[i]
@@ -36,3 +36,21 @@ console.log(minRemoveToMakeValid('a)b(c)d')) // ab(c)d
 console.log(minRemoveToMakeValid('))((')) // ''
 console.log(minRemoveToMakeValid('(a(b(c)d)')) // a(b(c)d)
 console.log(minRemoveToMakeValid('())()(((')) // ()()
+
+function minRemoveToMakeValid(s) {
+  let [stack, len] = [[], s.length]
+  s = s.split('')
+  for (let i = 0; i < len; i++) {
+    const val = s[i]
+    if (val === '(') {
+      stack.push(i)
+    } else if (val === ')') {
+      stack.length > 0 ? stack.pop() : s.splice(i, 1, '')
+    }
+  }
+  len = stack.length
+  for (let i = 0; i < len; i++) {
+    s.splice(stack[i], 1, '')
+  }
+  return s.join('')
+}
