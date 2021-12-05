4cc62cd2126610e6391ca1885e0e474a2f0dd50b
wumo1016 <760478279@qq.com>
2021-12-05 10:22:44 +0800

    chore(145)

--- leetcode/[145]二叉树的后序遍历.js
+++ leetcode/[145]二叉树的后序遍历.js
@@ -2,8 +2,38 @@
 - 给定一个二叉树，返回它的后序遍历
 */
 
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
-function postorderTraversal(root) {}
+function postorderTraversal(root) {
+  let res = []
+  const loop = node => {
+    if (node) {
+      const { val, left, right } = node
+      loop(left)
+      loop(right)
+      res.push(val)
+    }
+    return res
+  }
+  return loop(root)
+}
+
+const data = {
+  val: 1,
+  right: {
+    val: 2,
+    left: {
+      val: 3
+    }
+  }
+}
+
+console.log(postorderTraversal(data))
