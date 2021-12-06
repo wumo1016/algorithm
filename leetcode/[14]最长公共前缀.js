21467cabe7c10d448b5efe89ff49a64974f4c1fc
wumo1016 <760478279@qq.com>
2021-12-06 09:14:44 +0800

    feat(14)

---
+++ leetcode/[14]最长公共前缀.js
@@ -0,0 +1,14 @@
+/* 最长公共前缀(https://leetcode-cn.com/problems/longest-common-prefix/description/)
+- 编写一个函数来查找字符串数组中的最长公共前缀
+- 如果不存在公共前缀，返回空字符串""
+- strs[i] 仅由小写英文字母组成
+*/
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {}
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight'])) // fl
+console.log(longestCommonPrefix(['dog', 'racecar', 'car'])) // ''
