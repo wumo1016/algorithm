e0852e411d9b6c238d76db869add6caeaef1245b
wumo1016 <760478279@qq.com>
2021-12-07 11:08:53 +0800

    feat(3)

---
+++ leetcode/[3]无重复字符的最长子串.js
@@ -0,0 +1,14 @@
+/* 无重复字符的最长子串(https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/)
+- 给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function lengthOfLongestSubstring(s) {}
+
+console.log(lengthOfLongestSubstring('abcabcbb')) // 3
+console.log(lengthOfLongestSubstring('bbbbb')) // 1
+console.log(lengthOfLongestSubstring('pwwkew')) // 3
+console.log(lengthOfLongestSubstring('')) // 0
