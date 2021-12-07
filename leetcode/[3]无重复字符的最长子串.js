664016f340ab35db4ce0517b9694b6d44f188fa9
wumo1016 <760478279@qq.com>
2021-12-07 21:31:34 +0800

    chore(3)

--- leetcode/[3]无重复字符的最长子串.js
+++ leetcode/[3]无重复字符的最长子串.js
@@ -40,14 +40,38 @@ console.log(lengthOfLongestSubstring('aab')) // 2
 console.log(lengthOfLongestSubstring('dvdf')) // 3
 
 // 优化版
-function lengthOfLongestSubstring(s) {
-  let [len, start, max] = [s.length, 0, [0, 0]]
+function lengthOfLongestSubstring2(s) {
+  let [len, start, max] = [s.length, 0, 0]
   for (let i = 0; i < len; i++) {
     const p = s.slice(start, i).indexOf(s[i])
     if (p > -1) {
-      if (i - start > max[1] - max[0]) max = [start, i]
+      max = Math.max(i - start, max)
       start = p + start + 1
     }
   }
-  return Math.max(len - start, max[1] - max[0])
+  return Math.max(len - start, max)
+}
+
+// 利用滑动窗口
+function lengthOfLongestSubstring(s) {
+  // 哈希集合，记录每个字符是否出现过
+  const occ = new Set()
+  const n = s.length
+  // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+  let rk = -1,
+    ans = 0
+  for (let i = 0; i < n; ++i) {
+    if (i != 0) {
+      // 左指针向右移动一格，移除一个字符
+      occ.delete(s.charAt(i - 1))
+    }
+    while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
+      // 不断地移动右指针
+      occ.add(s.charAt(rk + 1))
+      ++rk
+    }
+    // 第 i 到 rk 个字符是一个极长的无重复字符子串
+    ans = Math.max(ans, rk - i + 1)
+  }
+  return ans
 }
