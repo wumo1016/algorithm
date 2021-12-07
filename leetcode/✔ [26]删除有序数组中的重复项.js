48330d624f45e6a14c46b35411816bb7159026f9
wumo1016 <760478279@qq.com>
2021-12-07 11:02:40 +0800

    chore(26)

--- leetcode/✔ [26]删除有序数组中的重复项.js
+++ leetcode/✔ [26]删除有序数组中的重复项.js
@@ -17,3 +17,14 @@ function removeDuplicates(nums) {
 }
 
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+
+// 只修改前面正确的部分
+function removeDuplicates(nums) {
+  let i = 0
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      nums[++i] = nums[j]
+    }
+  }
+  return i + 1
+}
