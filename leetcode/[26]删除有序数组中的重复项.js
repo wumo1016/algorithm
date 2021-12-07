80808cdfa08b50092165bd31c09896d0fca83be9
wumo1016 <760478279@qq.com>
2021-12-07 10:35:42 +0800

    chore(26)

---
+++ leetcode/[26]删除有序数组中的重复项.js
@@ -0,0 +1,11 @@
+/* 删除有序数组中的重复项(https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/description/)
+- 给你一个有序数组 nums ，请你 原地 删除重复出现的元素
+- 使每个元素 只出现一次 ，返回删除后数组的新长度
+- 不要使用额外的数组空间，你必须在原地修改输入数组 并在使用O(1)额外空间的条件下完成
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {}
