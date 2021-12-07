106bfd279e7e5e09b459750f195315a37e7bc7bf
wumo1016 <760478279@qq.com>
2021-12-07 21:07:49 +0800

    chore(3)

--- leetcode/[3]无重复字符的最长子串.js
+++ leetcode/[3]无重复字符的最长子串.js
@@ -5,33 +5,30 @@
 /**
  * @param {string} s
  * @return {number}
+ * 记录一个开始位置 然后记录当前的无重复子字符串
+ * 一旦出现重复的 拿到当前的长度与缓存的最大长度对比 如果比缓存的大 则替换缓存的
+ * 然后开始位置 是当前无重复子字符串的第一个向后移一位
  */
-function lengthOfLongestSubstring(s) {
-  if (!s) return 0
-  let start = 0,
-    res = [],
-    len = s.length,
-    cache = []
+function lengthOfLongestSubstring1(s) {
+  let [len, start, res, max] = [s.length, 0, [], []]
   for (let i = 0; i < len; i++) {
-    const cur = s.slice(start, i)
-    const index = cur.indexOf(s[i])
+    const index = s.slice(start, i).indexOf(s[i])
     if (index > -1) {
-      if (cache.length) {
-        if (res[1] - res[0] > cache[1] - cache[0]) {
-          cache = res
+      if (max.length) {
+        if (res[1] - res[0] > max[1] - max[0]) {
+          max = res
         }
       } else {
-        cache = res
+        max = res
       }
       start = index + start + 1
     } else {
       res = [start, i + 1]
     }
   }
-  if (start === 0) return len
-  if (cache.length < 1) return res[1] - res[0]
   res = [start, len]
-  return Math.max(res[1] - res[0], cache[1] - cache[0])
+  if (max.length < 1) return res[1] - res[0]
+  return Math.max(res[1] - res[0], max[1] - max[0])
 }
 
 console.log(lengthOfLongestSubstring('abcabcbb')) // 3
@@ -41,3 +38,15 @@ console.log(lengthOfLongestSubstring('')) // 0
 console.log(lengthOfLongestSubstring(' ')) // 1
 console.log(lengthOfLongestSubstring('aab')) // 2
 console.log(lengthOfLongestSubstring('dvdf')) // 3
+
+function lengthOfLongestSubstring(s) {
+  let [len, start, max] = [s.length, 0, [0, 0]]
+  for (let i = 0; i < len; i++) {
+    const p = s.slice(start, i).indexOf(s[i])
+    if (p > -1) {
+      if (i - start > max[1] - max[0]) max = [start, i]
+      start = p + start + 1
+    }
+  }
+  return Math.max(len - start, max[1] - max[0])
+}
