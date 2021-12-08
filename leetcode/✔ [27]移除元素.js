26b24d190dadf26bb967abae2eb3d4f77fa8482e
wumo1016 <760478279@qq.com>
2021-12-08 20:45:12 +0800

    chore(27)

--- leetcode/✔ [27]移除元素.js
+++ leetcode/✔ [27]移除元素.js
@@ -22,12 +22,25 @@ console.log(removeElement([3, 2, 2, 3], 3)) // 2
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)) // 5
 
 // 双指针
-function removeElement(nums, val) {
+function removeElement2(nums, val) {
   let [len, left] = [nums.length, 0]
   for (let i = 0; i < len; i++) {
     const right = nums[i]
     if (right !== val) {
       nums[left] = right
+      left++
+    }
+  }
+  return left
+}
+// 双指针优化: 防止了被保留元素的重复赋值
+function removeElement(nums, val) {
+  let [left, right] = [0, nums.length]
+  while (left < right) {
+    if (nums[left] === val) {
+      nums[left] = nums[right - 1]
+      right--
+    } else {
       left++
     }
   }
