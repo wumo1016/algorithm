4fe3a4ef3dcb8109f42ddb5ab2a99bd976216ec2
wumo1016 <760478279@qq.com>
2021-12-08 10:37:46 +0800

    feat(5)

---
+++ leetcode/[5]最长回文子串.js
@@ -0,0 +1,14 @@
+/* 最长回文子串(https://leetcode-cn.com/problems/longest-palindromic-substring/description/)
+- 给你一个字符串 s，找到s中最长的回文子串
+- s 仅由数字和英文字母（大写和/或小写）组成
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function longestPalindrome(s) {}
+
+console.log(longestPalindrome('cbbd')) // bb
+console.log(longestPalindrome('a')) // a
+console.log(longestPalindrome('ac')) // a
