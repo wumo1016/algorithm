4aa695ca42b1f4defbc8a5525fa4b8e212293a3c
wumo1016 <760478279@qq.com>
2021-12-08 20:32:28 +0800

    chore(5)

--- leetcode/✔ [5]最长回文子串.js
+++ leetcode/✔ [5]最长回文子串.js
@@ -7,7 +7,7 @@
  * @param {string} s
  * @return {string}
  */
-function longestPalindrome(s) {
+function longestPalindrome1(s) {
   let [len, res] = [s.length, '']
   for (let i = 0; i < len; i++) {
     if (len - i > res.length) {
@@ -43,3 +43,21 @@ console.log(
     'cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj'
   )
 ) // xrcrx
+
+// 中心扩散法
+function longestPalindrome(s) {
+  if (s.length < 2) return s
+  let res = ''
+  for (let i = 0; i < s.length; i++) {
+    helper(i, i) // 回文子串长度是奇数
+    helper(i, i + 1) // 回文子串长度是偶数
+  }
+  function helper(m, n) {
+    while (m >= 0 && n < s.length && s[m] == s[n]) {
+      m--
+      n++
+    }
+    if (n - m - 1 > res.length) res = s.slice(m + 1, n)
+  }
+  return res
+}
