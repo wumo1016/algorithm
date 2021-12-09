7b2f95a1701b9fe331ef15a2c1ec324a953f5205
wumo1016 <760478279@qq.com>
2021-12-09 17:39:57 +0800

    feat(28)

---
+++ leetcode/[28]实现 strStr().js
@@ -0,0 +1,16 @@
+/* 实现 strStr()(https://leetcode-cn.com/problems/implement-strstr/description/)
+- 给你两个字符串 haystack 和 needle
+- 请你在 haystack 字符串中找出 needle 字符串出现的第一个位置 下标从0开始
+- 如果不存在，则返回 -1
+*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+function strStr(haystack, needle) {}
+
+console.log(strStr('hello', 'll')) // 2
+console.log(strStr('aaaaa', 'bba')) // -1
+console.log(strStr('', '')) // 0
