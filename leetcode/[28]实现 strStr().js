5d5ae9c1a345372ff1d03f33009388e0bb0bfdd4
wumo1016 <760478279@qq.com>
2021-12-10 18:17:37 +0800

    chore

--- leetcode/[28]实现 strStr().js
+++ leetcode/[28]实现 strStr().js
@@ -9,9 +9,10 @@
  * @param {string} needle
  * @return {number}
  */
-function strStr(haystack, needle) {
+function strStr1(haystack, needle) {
   if (haystack === needle) return 0
   const [hLen, nLen] = [haystack.length, needle.length]
+  if (nLen === 0) return 0
   for (let i = 0; i < hLen && hLen - i >= nLen; i++) {
     if (haystack[i] === needle[0] && haystack.slice(i, i + nLen) === needle)
       return i
@@ -20,7 +21,36 @@ function strStr(haystack, needle) {
 }
 
 console.log(strStr('hello', 'll')) // 2
-console.log(strStr('aaaaa', 'bba')) // -1
-console.log(strStr('', '')) // 0
+// console.log(strStr('aaaaa', 'bba')) // -1
+// console.log(strStr('', '')) // 0
 
 // KMP算法 https://leetcode-cn.com/problems/implement-strstr/solution/kmpsuan-fa-js-by-xxxx__-fgi5/
+function strStr(haystack, needle) {
+  const n = haystack.length,
+    m = needle.length
+  if (m === 0) {
+    return 0
+  }
+  const pi = new Array(m).fill(0)
+  for (let i = 1, j = 0; i < m; i++) {
+    while (j > 0 && needle[i] !== needle[j]) {
+      j = pi[j - 1]
+    }
+    if (needle[i] == needle[j]) {
+      j++
+    }
+    pi[i] = j
+  }
+  for (let i = 0, j = 0; i < n; i++) {
+    while (j > 0 && haystack[i] != needle[j]) {
+      j = pi[j - 1]
+    }
+    if (haystack[i] == needle[j]) {
+      j++
+    }
+    if (j === m) {
+      return i - m + 1
+    }
+  }
+  return -1
+}
