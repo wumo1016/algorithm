391586d2bb2761ed9327a557fecd620a5d8914e8
wumo1016 <760478279@qq.com>
2021-12-11 15:08:00 +0800

    chore

--- leetcode/[35]搜索插入位置.js
+++ leetcode/[35]搜索插入位置.js
@@ -9,10 +9,31 @@
  * @param {number} target
  * @return {number}
  */
-function searchInsert(nums, target) {}
+function searchInsert(nums, target) {
+  let len = nums.length
+  let m = 0,
+    n = len - 1
+  while (m + 1 < n) {
+    const c = Math.ceil((n - m) / 2)
+    if (target > nums[c]) {
+      m = c
+    } else if (target < nums[c]) {
+      n = c
+    } else {
+      n = c
+      break
+    }
+    console.log(m, n, c);
+  }
+  if (target >= nums[m] && target <= nums[n]) return n
+  if (m === 0 && target <= nums[0]) return 0
+  if (n === len - 1 && target >= nums[len - 1]) return len
+}
 
-console.log(searchInsert([1, 3, 5, 6], 2)) // 1
-console.log(searchInsert([1, 3, 5, 6], 2)) // 1
-console.log(searchInsert([1, 3, 5, 6])) // 4
-console.log(searchInsert([1, 3, 5, 6], 0)) // 0
-console.log(searchInsert([1], 0)) // 0
+// console.log(searchInsert([1, 3, 5, 6], 2)) // 1
+// console.log(searchInsert([1, 3, 5, 7], 6)) // 3
+// console.log(searchInsert([1, 3, 5, 6], 7)) // 4
+// console.log(searchInsert([1, 3, 5, 6], 5)) // 2
+// console.log(searchInsert([1, 3, 5, 6], 0)) // 0
+// console.log(searchInsert([1], 0)) // 0
+console.log(searchInsert([1, 3], 1)) // 0
