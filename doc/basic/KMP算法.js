5c232b38c97655225c25b4ca887c4d91c298be51
wumo1016 <760478279@qq.com>
2021-12-11 09:12:52 +0800

    chore(kmp)

---
+++ doc/basic/KMP算法.js
@@ -0,0 +1,7 @@
+// https://www.zhihu.com/question/21923021/answer/281346746
+
+function strStr(haystack, needle) {}
+
+console.log(strStr('hello', 'll')) // 2
+// console.log(strStr('aaaaa', 'bba')) // -1
+// console.log(strStr('', '')) // 0
