348d923c2e932be37ac7ea62aa04624f7ba29940
wumo1016 <760478279@qq.com>
2021-12-13 20:18:00 +0800

    feat(11)

---
+++ leetcode/[11]盛最多水的容器.js
@@ -0,0 +1,14 @@
+/* 盛最多水的容器(https://leetcode-cn.com/problems/container-with-most-water/)
+- 给定一个非负整数数组 找出其中两个数 使他们索引的差与他们中的最小值的乘积最大
+*/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+function maxArea(height) {}
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])) // 49
+console.log(maxArea([1, 1])) // 1
+console.log(maxArea([4, 3, 2, 1, 4])) // 16
+console.log(maxArea([1, 2, 1])) // 2
