a2e013d163094b0608b9cd8093ec2fb1bb78fcfc
wumo1016 <760478279@qq.com>
2021-12-14 17:37:39 +0800

    feat(75)

---
+++ leetcode/[75]颜色分类.js
@@ -0,0 +1,16 @@
+/* 颜色分类(https://leetcode-cn.com/problems/sort-colors/description/)
+- 给定一个包含红色、白色和蓝色，一共 n 个元素的数组
+- 原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色
+- 1 <= nums.length <= 300
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums) {}
+
+console.log(sortColors([2, 0, 2, 1, 1, 0])) // [0,0,1,1,2,2]
+console.log(sortColors([2, 0, 1])) // [0,1,2]
+console.log(sortColors([0])) // [0]
+console.log(sortColors([1])) //
