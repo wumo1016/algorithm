dc05a6af5fd4a50d250ca7f5574388ecb18de465
wumo1016 <760478279@qq.com>
2021-12-14 18:08:48 +0800

    chore(75)

--- leetcode/[75]颜色分类.js
+++ leetcode/[75]颜色分类.js
@@ -1,6 +1,7 @@
 /* 颜色分类(https://leetcode-cn.com/problems/sort-colors/description/)
 - 给定一个包含红色、白色和蓝色，一共 n 个元素的数组
 - 原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色
+- 我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色
 - 1 <= nums.length <= 300
 */
 
@@ -8,12 +9,36 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-function sortColors(nums) {
+function sortColors1(nums) {
   nums.sort()
   console.log(nums)
 }
 
 sortColors([2, 0, 2, 1, 1, 0]) // [0,0,1,1,2,2]
-sortColors([2, 0, 1]) // [0,1,2]
-sortColors([0]) // [0]
-sortColors([1]) //
+// sortColors([2, 0, 2, 1, 1, 0]) // [0,0,1,1,2,2]
+// sortColors([2, 0, 1]) // [0,1,2]
+// sortColors([0]) // [0]
+// sortColors([1]) //
+
+function sortColors(nums) {
+  let [x, y, len] = [0, 0, nums.length]
+  for (let i = 0; i < len; i++) {
+    const value = nums[i]
+    if (value === 0) {
+      if (y === 0) {
+        nums[i] = nums[i - 1]
+        nums[x++] = value
+      } else {
+        nums[i] = nums[i - 1]
+        nums[y] = nums[x]
+        nums[x] = value
+      }
+    } else if (value === 1) {
+      if (y === 0) y = x
+      nums[i] = nums[y]
+      nums[y++] = value
+    }
+  }
+  console.log(x, y)
+  console.log(nums)
+}
