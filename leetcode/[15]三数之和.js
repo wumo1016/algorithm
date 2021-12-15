d3dbb4cd0a2eb43806c3d712457268f2545def52
wumo1016 <760478279@qq.com>
2021-12-15 17:29:07 +0800

    feat(15)

---
+++ leetcode/[15]三数之和.js
@@ -0,0 +1,15 @@
+/* 三数之和(https://leetcode-cn.com/problems/3sum/)
+- 给你一个包含 n 个整数的数组 nums
+- 判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0
+- 请你找出所有和为 0 且不重复的三元组
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function threeSum(nums) {}
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4])) // [[-1,-1,2],[-1,0,1]]
+console.log(threeSum([])) // []
+console.log(threeSum([0])) // []
