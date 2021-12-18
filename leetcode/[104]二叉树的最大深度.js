c87f2540098cbdc8f761e324aea4322fb0f10a11
wumo1016 <760478279@qq.com>
2021-12-18 20:37:13 +0800

    feat(104)

---
+++ leetcode/[104]二叉树的最大深度.js
@@ -0,0 +1,46 @@
+/* 二叉树的最大深度(https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/)
+- 给定一个二叉树，找出其最大深度。
+- 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+*/
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function maxDepth(root) {
+  let res = 0
+  function loop(root, max = 1) {
+    if (!root) return res
+    if (max > res) res = max
+    loop(root.left, max + 1)
+    loop(root.right, max + 1)
+    return res
+  }
+  return loop(root)
+}
+
+const data = {
+  val: 3,
+  left: {
+    val: 9
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15
+    },
+    right: {
+      val: 7
+    }
+  }
+}
+
+const data1 = {
+  val: 1,
+  right: {
+    val: 2
+  }
+}
+
+// console.log(maxDepth(data)) // 3
+console.log(maxDepth(data1)) // 2
