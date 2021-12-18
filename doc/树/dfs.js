bc28a33b164bf3aec824a2156116ab4feb1ae7f6
wumo1016 <760478279@qq.com>
2021-12-18 10:01:01 +0800

    feat(dfs)

---
+++ doc/树/dfs.js
@@ -0,0 +1,39 @@
+/* 深度优先遍历 */
+const tree = {
+  val: 'a',
+  children: [
+    {
+      val: 'b',
+      children: [
+        {
+          val: 'd',
+          children: []
+        },
+        {
+          val: 'e',
+          children: []
+        }
+      ]
+    },
+    {
+      val: 'c',
+      children: [
+        {
+          val: 'f',
+          children: []
+        },
+        {
+          val: 'g',
+          children: []
+        }
+      ]
+    }
+  ]
+}
+
+const dfs = root => {
+  console.log(root.val)
+  if (root.children?.length) root.children.forEach(dfs)
+}
+
+dfs(tree) // a => b => d => e > c => f > g
