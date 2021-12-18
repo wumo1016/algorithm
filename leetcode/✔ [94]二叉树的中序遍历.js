16fa6bd6d850b03624de82f447822a6a9bd0d6e0
wumo1016 <760478279@qq.com>
2021-12-18 19:31:56 +0800

    chore(94)

---
+++ leetcode/✔ [94]二叉树的中序遍历.js
@@ -0,0 +1,64 @@
+/* 二叉树的中序遍历(https://leetcode-cn.com/problems/binary-tree-inorder-traversal/)
+- 给定一个二叉树的根节点 root ，返回它的 中序 遍历
+*/
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+function postorderTraversal(root, res = []) {
+  if (!root) return res
+  postorderTraversal(root.left, res)
+  postorderTraversal(root.right, res)
+  res.push(root.val)
+  return res
+}
+
+const tree = {
+  val: 4,
+  left: {
+    val: 2,
+    left: {
+      val: 1,
+      left: null,
+      right: null
+    },
+    right: {
+      val: 3,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    val: 6,
+    left: {
+      val: 5,
+      left: null,
+      right: null
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null
+    }
+  }
+}
+
+// console.log(postorderTraversal(tree))
+console.log(postorderTraversal1(tree))
+
+// 非递归版
+function postorderTraversal1(root, res = []) {
+  if (!root) return res
+  let [stack, p] = [[], root]
+  while (stack.length || p) {
+    while (p) {
+      stack.push(p)
+      p = p.left
+    }
+    const last = stack.pop()
+    res.push(last.val)
+    p = last.right
+  }
+  return res
+}
