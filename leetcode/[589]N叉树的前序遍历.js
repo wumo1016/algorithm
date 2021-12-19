34291c8b0ba4399a6b43cd6df6db675a5fa917ea
wumo1016 <760478279@qq.com>
2021-12-19 16:18:23 +0800

    chore(589)

--- leetcode/[589]N叉树的前序遍历.js
+++ leetcode/[589]N叉树的前序遍历.js
@@ -1,4 +1,4 @@
-/* N 叉树的前序遍历(https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/)
+/* N叉树的前序遍历(https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/)
 - 给定一个 N 叉树，返回其节点值的 前序遍历
 */
 
