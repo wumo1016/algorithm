da061d6b4aa29ad9a2c4663205de436dfacfcf53
wumo1016 <760478279@qq.com>
2021-12-19 16:06:20 +0800

    chore(112)

--- leetcode/✔ [112]路径总和.js
+++ leetcode/✔ [112]路径总和.js
@@ -11,14 +11,40 @@
  * @param {number} targetSum
  * @return {boolean}
  */
+// dfs
+function hasPathSum(root, targetSum) {
+  if (!root) return false
+  let invalid = false
+  function dfs(root, sum = 0) {
+    if (!root || invalid) return
+    sum += root.val
+    if (!root.left && !root.right && sum === targetSum) invalid = true
+    dfs(root.left, sum)
+    dfs(root.right, sum)
+    return invalid
+  }
+  return dfs(root)
+}
+
+// bfs
 function hasPathSum(root, targetSum) {
   if (!root) return false
   const queue = [[root, root.val]]
   while (queue.length) {
-    const [{ left, right, val }, sum] = queue.pop()
+    const [{ left, right }, sum] = queue.pop()
     if (!left && !right && sum === targetSum) return true
     if (left) queue.unshift([left, sum + left.val])
     if (right) queue.unshift([right, sum + right.val])
   }
   return false
+}
+
+// dfs优化
+const hasPathSum = (root, sum) => {
+  if (root == null) return false
+  if (root.left == null && root.right == null) return sum - root.val == 0
+  return (
+    hasPathSum(root.left, sum - root.val) ||
+    hasPathSum(root.right, sum - root.val)
+  )
 }
