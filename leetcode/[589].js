f2e4ef7b0a8f8a6a8345fc5f3eaeb912b34e53b1
wumo1016 <760478279@qq.com>
2021-12-19 16:14:08 +0800

    chore(589)

--- leetcode/[589].js
+++ leetcode/[589].js
@@ -1,6 +1,18 @@
-/*
- * @Description: 
- * @Author: wyb
- * @LastEditors: wyb
- * @LastEditTime: 2021-12-17 16:42:11
+/* N 叉树的前序遍历(https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/)
+- 给定一个 N 叉树，返回其节点值的 前序遍历
+*/
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
  */
+function preorder(root) {
+  if (!root) return []
+  function dfs(root, res) {
+    if (!root) return
+    res.push(root.val)
+    ;(root.children || []).forEach(child => dfs(child, res))
+    return res
+  }
+  return dfs(root, [])
+}
