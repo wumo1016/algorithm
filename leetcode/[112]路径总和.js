003187856cf02fec9c9d8442cc65ce7376381b20
wumo1016 <760478279@qq.com>
2021-12-19 15:09:20 +0800

    feat(112)

---
+++ leetcode/[112]路径总和.js
@@ -0,0 +1,14 @@
+/* 路径总和(https://leetcode-cn.com/problems/path-sum/)
+- 给你二叉树的根节点 root 和一个表示目标和的整数targetSum
+- 判断该树中是否存在 根节点到叶子节点 的路径
+- 这条路径上所有节点值相加等于目标和 targetSum
+- 如果存在，返回 true ；否则，返回 false
+- 叶子节点 是指没有子节点的节点
+*/
+
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+function hasPathSum(root, targetSum) {}
