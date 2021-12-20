3254228fdc76df7029684a800c3afc07ee96ea73
wumo1016 <760478279@qq.com>
2021-12-20 15:27:21 +0800

    chore(226)

--- leetcode/[226]翻转二叉树.js
+++ leetcode/[226]翻转二叉树.js
@@ -1,3 +1,30 @@
-/* 翻转二叉树(https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/)
+/* 翻转二叉树(https://leetcode-cn.com/problems/invert-binary-tree/)
 - 翻转一棵二叉树
+- 就是树的左右节点互换
 */
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+function invertTree(root) {
+  const invert = root => {
+    if (root) {
+      ;[root.left, root.right] = [invert(root.right), invert(root.left)]
+    }
+    return root
+  }
+  return invert(root)
+}
+
+const data = {
+  val: 1,
+  left: {
+    val: 2
+  },
+  right: {
+    val: 3
+  }
+}
+
+console.log(invertTree(data))
