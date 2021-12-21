f6b00dd9b5c880795eb60d35538cc2d90c9cce08
wumo1016 <760478279@qq.com>
2021-12-21 18:08:30 +0800

    chore(110)

---
+++ leetcode/[110]平衡二叉树.js
@@ -0,0 +1,12 @@
+/* 平衡二叉树(https://leetcode-cn.com/problems/balanced-binary-tree/description/)
+- 给定一个二叉树，判断它是否是高度平衡的二叉树。
+- 判定标准是 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1
+*/
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isBalanced(root) {
+  if (!root) return true
+}
