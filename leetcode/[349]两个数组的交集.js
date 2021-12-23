37375d68d08d399e5b1de6f6f3176ba8b1d2621e
wumo1016 <760478279@qq.com>
2021-12-23 21:56:22 +0800

    feat(349)

---
+++ leetcode/[349]两个数组的交集.js
@@ -0,0 +1,15 @@
+/* 两个数组的交集(https://leetcode-cn.com/problems/intersection-of-two-arrays/)
+- 给定两个数组，编写一个函数来计算它们的交集
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+function intersection(nums1, nums2) {
+  return [...new Set(nums1)].filter(v => new Set(nums2).has(v))
+}
+
+console.log(intersection([1, 2, 2, 1], [2, 2])) // [2]
+console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4])) //[9,4]
