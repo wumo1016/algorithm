f3db2b96f0568fc4c2b5f4b3c377d3be23e60b82
wumo1016 <760478279@qq.com>
2021-12-25 10:53:24 +0800

    chore(349)

--- leetcode/✔ [349]两个数组的交集.js
+++ leetcode/✔ [349]两个数组的交集.js
@@ -7,9 +7,24 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-function intersection(nums1, nums2) {
+function intersection1(nums1, nums2) {
   return [...new Set(nums1)].filter(v => nums2.includes(v))
 }
 
 console.log(intersection([1, 2, 2, 1], [2, 2])) // [2]
 console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4])) //[9,4]
+
+function intersection(nums1, nums2) {
+  const [len1, len2, map, res] = [nums1.length, nums2.length, new Map(), []]
+  for (let i = 0; i < len1; i++) {
+    map.set(nums1[i], true)
+  }
+  for (let i = 0; i < len2; i++) {
+    const val = nums2[i]
+    if (map.has(val)) {
+      map.delete(val)
+      res.push(val)
+    }
+  }
+  return res
+}
