abb9ff104f08146f9b5dfc169c5713f04cb8b3c5
wumo1016 <760478279@qq.com>
2021-12-25 11:08:55 +0800

    chore(1)

--- leetcode/✔ [1]两数之和.js
+++ leetcode/✔ [1]两数之和.js
@@ -1,4 +1,4 @@
-/* 两数之和
+/* 两数之和(https://leetcode-cn.com/problems/two-sum/)
 - 给定一个整数数组 nums 和一个整数目标值 target
 - 在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标
 - 默认每种输入只会对应一个答案 且数组中同一个元素在答案里不能重复出现
@@ -29,7 +29,7 @@ const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log(twoSum(data, 8))
 console.log(twoSum1(data, 8))
 
-// 哈希解法
+// 哈希解法1
 function twoSum1(nums, target) {
   const map = new Map()
   const len = nums.length
@@ -39,5 +39,17 @@ function twoSum1(nums, target) {
       return [map.get(poor), i]
     }
     map.set(nums[i], i)
+  }
+}
+// 哈希解法2
+function twoSum2(nums, target) {
+  const map = new Map()
+  const len = nums.length
+  for (let i = 0; i < len; i++) {
+    const poor = target - nums[i]
+    if (map.has(nums[i]) && map.get(nums[i]) !== i) {
+      return [map.get(nums[i]), i]
+    }
+    map.set(poor, i)
   }
 }
