e49305a11b4647a8439532e28eee5841322f8212
wumo1016 <760478279@qq.com>
2021-12-28 20:51:18 +0800

    feat(347)

---
+++ leetcode/[347]前 K 个高频元素.js
@@ -0,0 +1,15 @@
+/* 前 K 个高频元素(https://leetcode-cn.com/problems/top-k-frequent-elements/)
+- 给定整数数组nums和整数k
+- 请你返回其中出现频率前k高的元素
+- 你可以按任意顺序返回答案
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+function topKFrequent(nums, k) {}
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2)) // [1,2]
+console.log(topKFrequent([1], 1)) // [1]
