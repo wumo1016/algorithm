27fb5846d771745575c6d3e394965121b22cc543
wumo1016 <760478279@qq.com>
2021-12-29 15:14:02 +0800

    chore(347)

--- leetcode/[347]前 K 个高频元素.js
+++ leetcode/[347]前 K 个高频元素.js
@@ -9,7 +9,17 @@
  * @param {number} k
  * @return {number[]}
  */
-function topKFrequent(nums, k) {}
+function topKFrequent(nums, k) {
+  const maps = {}
+  nums.forEach(v => {
+    if (typeof maps[v] === 'undefined') maps[v] = 0
+    maps[v]++
+  })
+  return Object.entries(maps)
+    .sort((a, b) => b[1] - a[1])
+    .map(v => Number(v[0]))
+    .slice(0, k)
+}
 
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2)) // [1,2]
 console.log(topKFrequent([1], 1)) // [1]
