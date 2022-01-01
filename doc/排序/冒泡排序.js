0e103bc4a75926ccb8894346a6949f6eaa473d0b
wumo1016 <760478279@qq.com>
2022-01-01 18:59:09 +0800

    chore(冒泡排序)

--- doc/排序/冒泡排序.js
+++ doc/排序/冒泡排序.js
@@ -1,22 +1,20 @@
 /* 冒泡排序
-
+- 比较所有交换顺序 如果后面比前面的大 则交换位置
+- 一轮下来 可以保存最后一个元素是最大的
+- 这样执行n-1轮 就可以完成排序
+- 时间复杂度 O(n^2)
  */
 
 function sort(list) {
-  const res = []
-  list.forEach(v => {
-    if (res.length > 0) {
-      let i = 0,
-        len = res.length
-      while (i < len && res[i] < v) {
-        i++
+  const len = list.length
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (list[j] > list[j + 1]) {
+        ;[list[j], list[j + 1]] = [list[j + 1], list[j]]
       }
-      res.splice(i, 0, v)
-    } else {
-      res.push(v)
     }
-  })
-  return res
+  }
+  return list
 }
 
 const data = [3, 4, 4, 5, 3, 21, 5, 6, 4, 3, 22, 5]
