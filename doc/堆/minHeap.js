df574515c568868fd38adb897bc62dab7f140bf7
wumo1016 <760478279@qq.com>
2022-01-01 17:46:08 +0800

    chore(最小堆)

--- doc/堆/minHeap.js
+++ doc/堆/minHeap.js
@@ -114,3 +114,115 @@ heap1.insert(0)
 heap1.pop()
 
 console.log(heap1.heap)
+
+// 简化版
+class MinHeap1 {
+  constructor() {
+    this.heap = []
+  }
+  get size() {
+    return this.heap.length
+  }
+  insert(value) {
+    this.heap.push(value)
+    this.up(this.heap.length - 1)
+  }
+  pop() {
+    this.heap[0] = this.heap.pop()
+    this.down(0)
+  }
+  peek() {
+    return this.heap[0]
+  }
+  up(index) {
+    if (index === 0) return
+    const parentIndex = this.getParentIndex(index)
+    if (this.heap[parentIndex] > this.heap[index]) {
+      this.swap(parentIndex, index)
+      this.up(parentIndex)
+    }
+  }
+  down(index) {
+    const leftIndex = this.getLeftChildIndex(index)
+    const rightIndex = this.getRightChildIndex(index)
+    if (this.heap[leftIndex] < this.heap[index]) {
+      this.swap(leftIndex, index)
+      this.down(leftIndex)
+    }
+    if (this.heap[rightIndex] < this.heap[index]) {
+      this.swap(rightIndex, index)
+      this.down(rightIndex)
+    }
+  }
+  getParentIndex(index) {
+    return (index - 1) >> 1
+  }
+  getLeftChildIndex(index) {
+    return index * 2 + 1
+  }
+  getRightChildIndex(index) {
+    return index * 2 + 2
+  }
+  swap(i1, i2) {
+    ;[this.heap[i1], this.heap[i2]] = [this.heap[i2], this.heap[i1]]
+  }
+}
+
+// 取值版
+class MinHeap2 {
+  constructor() {
+    this.heap = []
+  }
+  get size() {
+    return this.heap.length
+  }
+  insert(value) {
+    this.heap.push(value)
+    this.up(this.heap.length - 1)
+  }
+  pop() {
+    this.heap[0] = this.heap.pop()
+    this.down(0)
+  }
+  peek() {
+    return this.heap[0]
+  }
+  up(index) {
+    if (index === 0) return
+    const parentIndex = this.getParentIndex(index)
+    if (this.heap[parentIndex]['value'] > this.heap[index]['value']) {
+      this.swap(parentIndex, index)
+      this.up(parentIndex)
+    }
+  }
+  down(index) {
+    const leftIndex = this.getLeftChildIndex(index)
+    const rightIndex = this.getRightChildIndex(index)
+    if (
+      this.heap[leftIndex] &&
+      this.heap[leftIndex]['value'] < this.heap[index]['value']
+    ) {
+      this.swap(leftIndex, index)
+      this.down(leftIndex)
+    }
+    if (
+      this.heap[rightIndex] &&
+      this.heap[rightIndex]['value'] < this.heap[index]['value']
+    ) {
+      this.swap(rightIndex, index)
+      this.down(rightIndex)
+    }
+  }
+  getParentIndex(index) {
+    return (index - 1) >> 1
+  }
+  getLeftChildIndex(index) {
+    return index * 2 + 1
+  }
+  getRightChildIndex(index) {
+    return index * 2 + 2
+  }
+  swap(i1, i2) {
+    ;[this.heap[i1], this.heap[i2]] = [this.heap[i2], this.heap[i1]]
+  }
+}
