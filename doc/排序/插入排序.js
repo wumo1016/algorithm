941d76764ee81ffde5e26c5911b564e383a1fef4
wumo1016 <760478279@qq.com>
2022-01-02 10:01:53 +0800

    chore(插入排序)

---
+++ doc/排序/插入排序.js
@@ -0,0 +1,27 @@
+/* 插入排序
+- 从第二个元素开始遍历
+- 保存当前元素
+  - 遍历前面所有的元素(倒序) 如果比它大就往后移动一位
+  - 如果最终能找到比它小的 就插入当前元素
+ */
+
+function sort(list) {
+  const len = list.length
+  for (let i = 1; i < len; i++) {
+    const x = list[i]
+    let j = i
+    while (j > 0) {
+      if (list[j - 1] > x) {
+        list[j] = list[j - 1]
+      } else {
+        break
+      }
+      j--
+    }
+    if (j !== i) list[j] = x
+  }
+  return list
+}
+
+const data = [3, 4, 4, 5, 3, 1, 5, 6, 4, 3, 2, 5]
+console.log(sort(data))
