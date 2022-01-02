85b6cb4ad8ac5cd72bf4e769b9b76328bdc2f785
wumo1016 <760478279@qq.com>
2022-01-02 17:29:26 +0800

    feat(J40)

---
+++ leetcode/[J40]最小的k个数.js
@@ -0,0 +1,13 @@
+/* 最小的k个数(https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/)
+- 输入整数数组 arr 找出其中最小的k个数
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+function getLeastNumbers(arr, k) {}
+
+console.log(getLeastNumbers([3, 2, 1], 2)) // [1,2]
+console.log(getLeastNumbers([0, 1, 2, 1], 2)) // [0]
