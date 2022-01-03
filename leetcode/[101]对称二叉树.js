734c758395043a5e7a99e53f3a448720fb4491c9
wumo1016 <760478279@qq.com>
2022-01-03 17:21:37 +0800

    feat(101)

--- leetcode/[101]对称二叉树.js
+++ leetcode/[101]对称二叉树.js
@@ -1,9 +1,14 @@
-/* 对称二叉树(https://leetcode-cn.com/problems/symmetric-tree/)
-- 给定一个二叉树，检查它是否是镜像对称的。
+/* 爬楼梯(https://leetcode-cn.com/problems/climbing-stairs/)
+- 假设你正在爬楼梯。需要 n 阶你才能到达楼顶
+- 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+- 给定 n 是一个正整数
 */
 
 /**
- * @param {TreeNode} root
- * @return {boolean}
+ * @param {number} n
+ * @return {number}
  */
-function isSymmetric(root) {}
+function climbStairs(n) {}
+
+console.log(climbStairs(2)) // 2
+console.log(climbStairs(3)) // 3
