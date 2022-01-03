440e65d55b2f504f0fc865aca9251da4cb6f1e1f
wumo1016 <760478279@qq.com>
2022-01-03 17:23:33 +0800

    chore(70)

---
+++ leetcode/[70]爬楼梯.js
@@ -0,0 +1,14 @@
+/* 爬楼梯(https://leetcode-cn.com/problems/climbing-stairs/)
+- 假设你正在爬楼梯。需要 n 阶你才能到达楼顶
+- 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+- 给定 n 是一个正整数
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ function climbStairs(n) {}
+
+ console.log(climbStairs(2)) // 2
+ console.log(climbStairs(3)) // 3
