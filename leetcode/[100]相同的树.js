47eb947a394735753be27a2a01b1b2b3b08dad04
wumo1016 <760478279@qq.com>
2022-01-03 17:16:47 +0800

    chore(100)

---
+++ leetcode/[100]相同的树.js
@@ -0,0 +1,11 @@
+/* 相同的树(https://leetcode-cn.com/problems/same-tree/)
+- 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同
+- 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的
+*/
+
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+function isSameTree(p, q) {}
