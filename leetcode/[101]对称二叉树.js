89629a27a0c87363669b4a3d1ab1bcbe19166d23
wumo1016 <760478279@qq.com>
2022-01-06 11:33:01 +0800

    chore(101)

--- leetcode/[101]对称二叉树.js
+++ leetcode/[101]对称二叉树.js
@@ -6,4 +6,25 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-function isSymmetric(root) {}
+// 左子树-中左右 右子树-中右左 然后对比
+function isSymmetric(root) {
+  let leftList = []
+  function loop1(root) {
+    if (root === null) leftList.push(null)
+    if (!root) return
+    leftList.push(root.val)
+    loop1(root.left)
+    loop1(root.right)
+  }
+  loop1(root.left)
+  let rightList = []
+  function loop2(root) {
+    if (root === null) rightList.push(null)
+    if (!root) return
+    rightList.push(root.val)
+    loop2(root.right)
+    loop2(root.left)
+  }
+  loop2(root.right)
+  return leftList.toString() === rightList.toString()
+}
