4f1fecbd10cde57b11b8f4cdc378070d5656d3af
wumo1016 <760478279@qq.com>
2022-01-09 19:04:26 +0800

    chore

--- leetcode/[1470]重新排列数组.js
+++ leetcode/[1470]重新排列数组.js
@@ -11,9 +11,17 @@
  */
 function shuffle(nums, n) {
   const nums1 = nums.slice(n)
-  return nums.slice(0, n).reduce((res, cur, index) => res.concat(cur, nums1[index]), [])
+  return nums
+    .slice(0, n)
+    .reduce((res, cur, index) => res.concat(cur, nums1[index]), [])
 }
 
 console.log(shuffle([2, 5, 1, 3, 4, 7], 3)) // [2,3,5,4,1,7]
 console.log(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4)) // [1,4,2,3,3,2,4,1]
 console.log(shuffle([1, 1, 2, 2], 2)) // [1,2,1,2]
+
+function shuffle(nums, n) {
+  return Array(n)
+    .fill()
+    .reduce((res, cur, index) => res.concat(nums[index], nums[index + n]), [])
+}
