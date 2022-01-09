9273b5afd870f4e73d684b49b2abcaefe318831a
wumo1016 <760478279@qq.com>
2022-01-09 15:40:15 +0800

    chore(100)

--- leetcode/[100]相同的树.js
+++ leetcode/[100]相同的树.js
@@ -26,3 +26,16 @@ function isSameTree(p, q) {
   loop(p, q)
   return valid
 }
+
+function isSameTree(p, q) {
+  if (!q && !p) return true
+  if (
+    p &&
+    q &&
+    p.val === q.val &&
+    isSameTree(p.left, q.left) &&
+    isSameTree(p.right, q.right)
+  )
+    return true
+  return false
+}
