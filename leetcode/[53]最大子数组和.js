087ae0d8f2eab97ea77ed7ca2dcd2de745c77edb
wumo1016 <760478279@qq.com>
2022-01-14 16:11:21 +0800

    feat(35)

---
+++ leetcode/[53]最大子数组和.js
@@ -0,0 +1,16 @@
+/* 最大子数组和(https://leetcode-cn.com/problems/maximum-subarray/description/)
+- 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组
+- 子数组最少包含一个元素 返回其最大和
+- 子数组是数组中的一个连续部分
+- 1 <= nums.length <= 105
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maxSubArray(nums) {}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])) // [4,-1,2,1] 6
+// console.log(maxSubArray([5, 4, -1, 7, 8])) // 23
+// console.log(maxSubArray([1])) // 1
