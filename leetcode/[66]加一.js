d6228e98f18edb7bdd2b3a5abccc977d60f9d5a7
wumo1016 <760478279@qq.com>
2022-01-14 16:36:22 +0800

    chore(66)

--- leetcode/[66]加一.js
+++ leetcode/[66]加一.js
@@ -28,3 +28,4 @@ function plusOne(digits) {
 
 console.log(plusOne([1, 2, 3])) // [1,2,4]
 console.log(plusOne([4, 3, 2, 1])) // [4,3,2,2]
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3])) // [4,3,2,2]
