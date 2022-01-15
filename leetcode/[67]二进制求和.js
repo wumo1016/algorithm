64c77cb25b3f9c7558aad1f9565c767c807de8a7
wumo1016 <760478279@qq.com>
2022-01-15 09:30:32 +0800

    feat(67)

---
+++ leetcode/[67]二进制求和.js
@@ -0,0 +1,14 @@
+/* 二进制求和(https://leetcode-cn.com/problems/add-binary/description/)
+- 给你两个二进制字符串，返回它们的和（用二进制表示）
+- 输入为 非空 字符串且只包含数字 1 和 0
+*/
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+function addBinary(a, b) {}
+
+console.log(addBinary('11', '1')) // 100
+console.log(addBinary('1010', '1011')) // 10101
