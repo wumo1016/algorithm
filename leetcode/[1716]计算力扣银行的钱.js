e2e298d7868ff3867798f3a7a3c11ff614c1e769
wumo1016 <760478279@qq.com>
2022-01-15 14:44:53 +0800

    chore(1716)

---
+++ leetcode/[1716]计算力扣银行的钱.js
@@ -0,0 +1,33 @@
+/* 计算力扣银行的钱(https://leetcode-cn.com/problems/calculate-money-in-leetcode-bank/)
+- Hercy 想要为购买第一辆车存钱。他 每天 都往力扣银行里存钱
+- 最开始，他在周一的时候存入 1 块钱。从周二到周日，他每天都比前一天多存入 1 块钱
+- 在接下来每一个周一，他都会比 前一个周一 多存入 1 块钱。
+- 给你 n ，请你返回在第 n 天结束的时候他在力扣银行总共存了多少块钱
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function totalMoney(n) {
+  let [x, y, res] = [(n / 7) >> 0, n % 7, 0]
+  for (let i = 1; i <= x; i++) {
+    res += ((2 * i + 6) * 7) / 2
+  }
+  res += ((x + 1 + x + 1 + y - 1) * y) / 2
+  return res
+}
+
+console.log(totalMoney(4)) // 10
+console.log(totalMoney(10)) // 37
+console.log(totalMoney(20)) // 96
+
+// 简化版
+function totalMoney(n) {
+  let [x, y, res] = [(n / 7) >> 0, n % 7, 0]
+  for (let i = 1; i <= x; i++) {
+    res += ((2 * i + 6) * 7) / 2
+  }
+  res += ((2 * x + 1 + y) * y) / 2
+  return res
+}
