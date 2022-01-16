1a27ee54518ffc95d19874a57aec6948348551e7
wumo1016 <760478279@qq.com>
2022-01-16 14:56:39 +0800

    chore(198)

--- leetcode/[198]打家劫舍.js
+++ leetcode/[198]打家劫舍.js
@@ -3,13 +3,27 @@
 - 每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统
 - 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警
 - 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额
+- 1 <= nums.length <= 100
 */
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
-function rob(nums) {}
+// 动态规划
+// f(k) = Math.max(f(k - 2 + cur), f(k - 1))
+function rob(nums) {
+  const len = nums.length
+  if (len < 3) return Math.max(...nums)
+  let [pre1, pre2] = [nums[0], Math.max(nums[0], nums[1])]
+  for (let i = 2; i < len; i++) {
+    const temp = pre1
+    pre1 = pre2
+    pre2 = Math.max(temp + nums[i], pre2)
+  }
+  return pre2
+}
 
 console.log(rob([1, 2, 3, 1])) // 4
 console.log(rob([2, 7, 9, 3, 1])) // 12
+console.log(rob([2, 1, 1, 2])) // 4
