2d45eaaf5530a6b85846074e440bda1729048aff
wumo1016 <760478279@qq.com>
2022-01-17 15:16:06 +0800

    chore(200)

--- leetcode/[200]岛屿数量.js
+++ leetcode/[200]岛屿数量.js
@@ -9,7 +9,27 @@
  * @param {character[][]} grid
  * @return {number}
  */
-function numIslands(grid) {}
+function numIslands(grid) {
+  const [mLen, nLen] = [grid.length, grid[0].length]
+  function setZero(i, j) {
+    if (i < 0 || j < 0 || i >= mLen || j >= nLen || grid[i][j] === '0') return
+    grid[i][j] = '0'
+    setZero(i - 1, j)
+    setZero(i, j + 1)
+    setZero(i + 1, j)
+    setZero(i, j - 1)
+  }
+  let res = 0
+  for (let i = 0; i < mLen; i++) {
+    for (let j = 0; j < nLen; j++) {
+      if (grid[i][j] === '1') {
+        setZero(i, j)
+        res++
+      }
+    }
+  }
+  return res
+}
 
 console.log(
   numIslands([
