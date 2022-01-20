3a34bd0d8d61730271c04a4ae5808222b29a9d43
wumo1016 <760478279@qq.com>
2022-01-20 21:39:04 +0800

    chore(并查集)

--- doc/并查集/quickUnion.js
+++ doc/并查集/quickUnion.js
@@ -2,41 +2,11 @@
  * @Description: 并查集
  * @Author: wyb
  * @LastEditors: wyb
- * @LastEditTime: 2022-01-20 13:42:36
+ * @LastEditTime: 2022-01-20 21:36:53
  */
 
-class QuickUnion {
-  constructor(n = 100) {
-    this.n = n
-    this.size = n // 当前的集合数
-    this.father = Array(n)
-    this.init()
-  }
-  // 将每个节点的颜色初始化为自身颜色
-  init() {
-    for (let i = 0; i < this.n; i++) {
-      this.father[i] = i
-    }
-  }
-  // 查找
-  find(i) {
-    return this.father[i] === i ? i : this.find(this.father[i])
-  }
-  // 是否是同一集合
-  same(x, y) {
-    return this.find(x) === this.find(y)
-  }
-  // 合并
-  merge(x, y) {
-    const [m, n] = [this.find(x), this.find(y)]
-    if (m === n) return false
-    this.father[m] = n
-    this.size--
-    return true
-  }
-}
-
-class UnionSet {
+// 普通版
+class UnionSet1 {
   constructor(n = 100) {
     this.size = n
     this.father = Array(n)
@@ -57,3 +27,36 @@ class UnionSet {
     return true
   }
 }
+// 优化版 - 按秩合并(将节点数少的合并到节点数多的上面去)
+class UnionSet2 {
+  constructor(n = 100) {
+    this.father = Array(n)
+    this.size = Array(n) // 储存当前集合的元素数量
+    for (let i = 0; i < n; i++) {
+      this.father[i] = i
+      this.size[i] = 1
+    }
+    console.log(this.father)
+    console.log(this.size)
+  }
+  find(i) {
+    return this.father[i] === i ? i : this.find(this.father[i])
+  }
+  isSame(x, y) {
+    return this.find(x) === this.find(y)
+  }
+  merge(x, y) {
+    const [m, n] = [this.find(x), this.find(y)]
+    if (m === n) return false
+    if (this.size[m] < this.size[n]) {
+      this.father[m] = n
+      this.size[n] += this.size[m]
+    } else {
+      this.father[n] = m
+      this.size[m] += this.size[n]
+    }
+    return true
+  } 
+}
+
+new UnionSet2()
