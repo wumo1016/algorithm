f8aeb0aba5bdd3e9c8198027178524f14069ff9c
wumo1016 <760478279@qq.com>
2022-01-20 10:18:54 +0800

    chore(1319)

--- leetcode/[1319]连通网络的操作次数.js
+++ leetcode/[1319]连通网络的操作次数.js
@@ -11,27 +11,27 @@ class QuickUnion {
     this.father = Array(n)
     this.init()
   }
-  // 将每个节点的颜色初始化为自身颜色
   init() {
     for (let i = 0; i < this.n; i++) {
       this.father[i] = i
     }
   }
-  // 查找
   find(i) {
     if (this.father[i] === i) return i
     return this.find(this.father[i])
   }
-  // 是否是同一集合
   same(x, y) {
     return this.find(x) === this.find(y)
   }
-  // 合并
   merge(x, y) {
     const m = this.find(x)
     const n = this.find(y)
     if (m === n) return true
     this.father[m] = n
+  }
+  get fatherNum() {
+    return [...new Set(Object.keys(this.father).map((_, i) => this.find(i)))]
+      .length
   }
 }
 
@@ -47,14 +47,7 @@ function makeConnected(n, connections) {
     if (quickUnion.merge(value[0], value[1])) num++
   }
   // 需要联通的线缆数
-  const need =
-    [
-      ...new Set(
-        Array(n)
-          .fill('')
-          .map((_, i) => quickUnion.find(i))
-      )
-    ].length - 1
+  const need = quickUnion.fatherNum - 1
   return need <= num ? need : -1
 }
 
