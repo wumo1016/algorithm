f164511f5f898f6df39c0fc02da5345c8e03ea25
wumo1016 <760478279@qq.com>
2022-01-21 13:06:45 +0800

    feat(121)

---
+++ leetcode/[121]买卖股票的最佳时机.js
@@ -0,0 +1,15 @@
+/* 买卖股票的最佳时机(https://leetcode-cn.com/problems/two-sum/)
+- 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+- 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
+- 设计一个算法来计算你所能获取的最大利润。
+- 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0。
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+function maxProfit(prices) {}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])) // 5
+console.log(maxProfit([7, 6, 4, 3, 1])) // 0
