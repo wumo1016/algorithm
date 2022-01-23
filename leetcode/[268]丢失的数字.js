ad25be1e6f781c6a2d126f4e44b2a3a5ac40d324
wumo1016 <760478279@qq.com>
2022-01-23 10:06:10 +0800

    chore(268)

--- leetcode/[268]丢失的数字.js
+++ leetcode/[268]丢失的数字.js
@@ -6,10 +6,11 @@
  * @param {number[]} nums
  * @return {number}
  */
+// O(n*logN)
 function missingNumber(nums) {
   nums.sort((a, b) => a - b)
-  const last = nums.length + 1
-  for (let i = 0; i < last; i++) {
+  const len = nums.length + 1
+  for (let i = 0; i < len; i++) {
     if (nums[i] !== i) return i
   }
 }
@@ -17,3 +18,15 @@ function missingNumber(nums) {
 console.log(missingNumber([3, 0, 1])) // 2
 console.log(missingNumber([0, 1])) // 2
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])) // 8
+
+// O(n)
+function missingNumber(nums) {
+  let [len, list] = [nums.length, []]
+  for (let i = 0; i < len; i++) {
+    list[nums[i]] = nums[i]
+  }
+  len++
+  for (let i = 0; i < len; i++) {
+    if (list[i] === undefined) return i
+  }
+}
