c0ec7c2f3688722f2bed1da14a7a62b2ee74d6ce
wumo1016 <760478279@qq.com>
2022-01-23 15:45:18 +0800

    feat(46)

---
+++ leetcode/[46]全排列.js
@@ -0,0 +1,17 @@
+/* 全排列(https://leetcode-cn.com/problems/permutations/)
+- 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+- nums 中的所有整数 互不相同
+- 1 <= nums.length <= 6
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function permute(nums) {
+  const len = nums.length
+}
+
+console.log(permute([1, 2, 3])) // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+console.log(permute([0, 1])) // [[0,1],[1,0]]
+console.log(permute([1])) // [[1]]
