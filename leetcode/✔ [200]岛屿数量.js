1bac5c2db58e1565850e43c3b306b78305be58bd
wumo1016 <760478279@qq.com>
2022-01-23 14:21:44 +0800

    chore(200)

--- leetcode/✔ [200]岛屿数量.js
+++ leetcode/✔ [200]岛屿数量.js
@@ -47,3 +47,58 @@ console.log(
     ['0', '0', '0', '1', '1']
   ])
 ) // 3
+console.log(
+  numIslands([
+    ['1', '1', '0', '0', '0'],
+    ['1', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0']
+  ])
+) // 3
+
+// 并查集
+function numIslands(grid) {
+  class UnionSet {
+    constructor(n = 100) {
+      this.n = n
+      this.father = new Array(n)
+      this.init(n)
+    }
+    init(n) {
+      for (let i = 0; i < n; i++) this.father[i] = i
+    }
+    find(x) {
+      return (this.father[x] =
+        this.father[x] == x ? x : this.find(this.father[x]))
+    }
+    merge(a, b) {
+      const fa = this.find(a) // a的根节点
+      const fb = this.find(b) // b的根节点
+      if (fa === fb) return false // 已经在一个集合中
+      this.father[fa] = fb
+      return true
+    }
+  }
+
+  let row = grid.length
+  let col = grid[0].length
+  const u = new UnionSet(row * col)
+  let count = 0
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] == '0') continue
+      count++
+      // 右
+      if (j + 1 < col && grid[i][j + 1] == '1') {
+        u.merge(i * col + j, i * col + j + 1) && count--
+      }
+      // 下
+      if (i + 1 < row && grid[i + 1][j] == '1') {
+        u.merge(i * col + j, (i + 1) * col + j) && count--
+      }
+    }
+  }
+
+  return count
+}
