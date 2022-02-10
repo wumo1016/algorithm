5c02e1c1e2943d3515390e740728e7fdc4ef3fdd
wumo1016 <760478279@qq.com>
2022-02-10 15:25:26 +0800

    feat(17)

---
+++ leetcode/[17]电话号码的字母组合.js
@@ -0,0 +1,16 @@
+/* 电话号码的字母组合(https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/)
+- 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+- 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+- 0 <= digits.length <= 4
+- digits[i] 是范围 ['2', '9'] 的一个数字。
+*/
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+function letterCombinations(digits) {}
+
+console.log(letterCombinations('23')) // ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+console.log(letterCombinations('')) // []
+console.log(letterCombinations('2')) // ["a","b","c"]
