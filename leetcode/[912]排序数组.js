85b11ffa512f7c8b3ebfdfa3dccf2eba4ff8cc4b
wumo1016 <760478279@qq.com>
2022-02-14 14:29:59 +0800

    feat(912)

---
+++ leetcode/[912]排序数组.js
@@ -0,0 +1,14 @@
+/* 排序数组(https://leetcode-cn.com/problems/sort-an-array/)
+- 给你一个整数数组 nums，请你将该数组升序排列
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function sortArray(nums) {
+  return nums.sort((a, b) => a - b)
+}
+
+console.log(sortArray([5, 2, 3, 1])) // [1,2,3,5]
+console.log(sortArray([5, 1, 1, 2, 0, 0])) // [0,0,1,1,2,5]
