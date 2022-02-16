d653c26fa31d66be5b3a4f52cc56d8976ed39dd8
wumo1016 <760478279@qq.com>
2022-02-16 15:14:45 +0800

    chore(912)

--- leetcode/[912]排序数组.js
+++ leetcode/[912]排序数组.js
@@ -14,16 +14,24 @@ console.log(sortArray([5, 2, 3, 1])) // [1,2,3,5]
 console.log(sortArray([5, 1, 1, 2, 0, 0])) // [0,0,1,1,2,5]
 
 // 冒泡排序
-function sortArray(nums) {}
+function sortArray(nums) {
+  const len = nums.length
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (nums[j] > nums[j + 1]) [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+    }
+  }
+  return nums
+}
 
 // 插入排序
-function sortArray(nums) {}
+// function sortArray(nums) {}
 
-// 快速排序(内存溢出)
-function sortArray(nums) {}
+// // 快速排序(内存溢出)
+// function sortArray(nums) {}
 
-// 归并排序
-function sortArray(nums) {}
+// // 归并排序
+// function sortArray(nums) {}
 
-// 选择排序
-function sortArray(nums) {}
+// // 选择排序
+// function sortArray(nums) {}
