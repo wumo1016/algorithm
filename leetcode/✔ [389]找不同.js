d3552fc07dc3e7c8ca1f57fa84b9bc85742bae71
wumo1016 <760478279@qq.com>
2022-02-20 10:13:19 +0800

    feat(389)

---
+++ leetcode/✔ [389]找不同.js
@@ -0,0 +1,43 @@
+/* 找不同(https://leetcode-cn.com/problems/find-the-difference/)
+- 给定两个字符串 s 和 t ，它们只包含小写字母
+- 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母
+- 请找出在 t 中被添加的字母
+*/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+function findTheDifference(s, t) {
+  const [len, map] = [t.length, new Map()]
+  for (let i = 0; i < len; i++) {
+    const [sVal, tVal] = [s[i], t[i]]
+    map.set(tVal, (map.get(tVal) || 0) + 1)
+    sVal && map.set(sVal, (map.get(sVal) || 0) - 1)
+    if (map.get(tVal) === 0) map.delete(tVal)
+    if (map.get(sVal) === 0) map.delete(sVal)
+  }
+  for (const key of map.keys()) {
+    return key
+  }
+}
+
+console.log(findTheDifference('abcd', 'abcde')) // e
+console.log(findTheDifference('', 'y')) // y
+console.log(
+  findTheDifference(
+    'ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor',
+    'qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj'
+  )
+) // t
+
+// 使用编码
+function findTheDifference(s, t) {
+  let [len, res] = [s.length, 0]
+  for (let i = 0; i < len; i++) {
+    res += t[i].charCodeAt() - s[i].charCodeAt()
+  }
+  res += t[t.length - 1].charCodeAt()
+  return String.fromCharCode(res)
+}
