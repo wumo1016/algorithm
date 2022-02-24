60e211d07590292a9d7fa2903bcf4c66c147609b
wumo1016 <760478279@qq.com>
2022-02-24 16:52:11 +0800

    chore(17.04)

--- leetcode/✔ [MST17.04]消失的数字.js
+++ leetcode/✔ [MST17.04]消失的数字.js
@@ -18,3 +18,11 @@ function missingNumber(nums) {
 console.log(missingNumber([0])) // 1
 console.log(missingNumber([3, 0, 1])) // 2
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])) // 8
+
+function missingNumber(nums) {
+  let [res, len] = [0, nums.length]
+  for (let i = 0; i < len; i++) {
+    res = res ^ i ^ nums[i]
+  }
+  return res ^ len
+}
