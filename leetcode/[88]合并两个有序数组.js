bd1c4656ad102b4f059b583538b6db76a4ba8c81
wumo1016 <760478279@qq.com>
2022-03-02 13:01:37 +0800

    chore(88)

--- leetcode/[88]合并两个有序数组.js
+++ leetcode/[88]合并两个有序数组.js
@@ -19,13 +19,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1, m, nums2, n) {
-  const res = []
-  let total = nums1.length,
-    r = 0
+  let [res, total, r] = [[], nums1.length, 0]
   for (let i = 0; i < total; i++) {
-    const val1 = nums1[i]
-    const val2 = nums2[r]
-
+    const [val1, val2] = [nums1[i], nums2[r]]
     if (res.length) {
       if (r < n) {
         if (val2 < res[0]) {
@@ -59,3 +55,56 @@ function merge(nums1, m, nums2, n) {
 // console.log(merge([0], 0, [1], 1)) // [1]
 // console.log(merge([1, 3, 4, 7, 0, 0, 0], 4, [2, 5, 6], 3)) // [1,2,2,3,5,6]
 console.log(merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3)) // [-1,0,0,1,2,2,3,3,3]
+
+// 优化版
+function merge(nums1, m, nums2, n) {
+  let [res, total, r] = [[], nums1.length, 0]
+  for (let i = 0; i < total; i++) {
+    const [val1, val2] = [nums1[i], nums2[r]]
+    if (res.length) {
+      if (r < n && val2 < res[0]) {
+        nums1[i] = val2
+        r++
+      } else {
+        nums1[i] = res.shift()
+      }
+      if (i < m) res.push(val1)
+    } else {
+      if (i < m && r < n) {
+        if (val2 < val1) {
+          nums1[i] = val2
+          r++
+          res.push(val1)
+        }
+      } else if (r < n) {
+        nums1[i] = val2
+        r++
+      }
+    }
+  }
+  return nums1
+}
+
+// 双指针
+function merge(nums1, m, nums2, n) {
+  let p1 = 0,
+    p2 = 0
+  const sorted = new Array(m + n).fill(0)
+  var cur
+  while (p1 < m || p2 < n) {
+    if (p1 === m) {
+      cur = nums2[p2++]
+    } else if (p2 === n) {
+      cur = nums1[p1++]
+    } else if (nums1[p1] < nums2[p2]) {
+      cur = nums1[p1++]
+    } else {
+      cur = nums2[p2++]
+    }
+    sorted[p1 + p2 - 1] = cur
+  }
+  for (let i = 0; i != m + n; ++i) {
+    nums1[i] = sorted[i]
+  }
+  return nums1
+}
