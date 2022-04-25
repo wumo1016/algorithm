a6fcf771f03ecfc920663caed289abdf5d9a0690
wumo1016 <760478279@qq.com>
2022-04-25 09:46:00 +0800

    chore(MST02.06)

--- leetcode/[MST02.06]回文链表.js
+++ leetcode/[MST02.06]回文链表.js
@@ -1,3 +1,7 @@
+/* 回文链表(https://leetcode-cn.com/problems/palindrome-linked-list-lcci/)
+- 编写一个函数，检查输入的链表是否是回文的。
+*/
+
 /**
  * @param {ListNode} head
  * @return {boolean}
@@ -35,3 +39,29 @@ function isPalindrome(head) {
 }
 
 // 快慢指针 + 反转链表 => 时间复杂度O(n) 空间复杂度O(1)
+function isPalindrome(head) {
+  if (!head || !head.next) return true
+  const reverseList = head => {
+    let prev = null
+    while (head) {
+      const next = head.next
+      head.next = prev
+      prev = head
+      head = next
+    }
+    return prev
+  }
+  let [fast, slow] = [head, head]
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
+  }
+  if (fast) slow = slow.next
+  slow = reverseList(slow)
+  while (slow) {
+    if (head.val !== slow.val) return false
+    head = head.next
+    slow = slow.next
+  }
+  return true
+}
