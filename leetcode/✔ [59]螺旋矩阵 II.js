8ea7e846caae822e518ce3c50440a85824699fb5
wumo1016 <760478279@qq.com>
2022-06-21 13:45:16 +0800

    chore(59)

--- leetcode/✔ [59]螺旋矩阵 II.js
+++ leetcode/✔ [59]螺旋矩阵 II.js
@@ -44,3 +44,29 @@ function generateMatrix(n) {
 }
 
 console.log(generateMatrix(3)) // [[1,2,3],[8,9,4],[7,6,5]]
+
+// 按层遍历
+function generateMatrix(n) {
+  const matrix = Array(n)
+    .fill()
+    .map(v => Array(n).fill(0))
+  let [left, right, top, bottom, val] = [0, n - 1, 0, n - 1, 1]
+  while (left <= right && top <= bottom) {
+    for (let i = left; i <= right; i++) {
+      matrix[top][i] = val++
+    }
+    for (let i = top + 1; i <= bottom; i++) {
+      matrix[i][right] = val++
+    }
+    if (bottom > top && right > left) {
+      for (let i = right - 1; i >= left; i--) {
+        matrix[bottom][i] = val++
+      }
+      for (let i = bottom - 1; i > top; i--) {
+        matrix[i][left] = val++
+      }
+    }
+    ;[left, right, top, bottom] = [left + 1, right - 1, top + 1, bottom - 1]
+  }
+  return matrix
+}
