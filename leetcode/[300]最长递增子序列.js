c48e0f82dafaacabe39f4d41785615c948f9ece9
wumo1016 <760478279@qq.com>
2022-06-22 10:06:55 +0800

    chore(300)

--- leetcode/[300]最长递增子序列.js
+++ leetcode/[300]最长递增子序列.js
@@ -9,17 +9,11 @@
  */
 // 动态规划 => 返回最长递增长度
 function lengthOfLIS(nums) {
-  const [len, res, list] = [nums.length, [1], [[0]]]
+  const [len, res] = [nums.length, [1]]
   for (let i = 1; i < len; i++) {
     res[i] = 1
-    list[i] = [i]
     for (let j = 0; j < i; j++) {
-      if (nums[j] < nums[i]) {
-        if (res[j] + 1 > res[i]) {
-          res[i] = res[j] + 1
-          list[i] = list[j].concat(i)
-        }
-      }
+      if (nums[j] < nums[i]) res[i] = Math.max(res[i], res[j] + 1)
     }
   }
   return Math.max(...res)
@@ -28,20 +22,3 @@ function lengthOfLIS(nums) {
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])) // 4
 console.log(lengthOfLIS([0, 1, 0, 3, 2, 3])) // 4
 console.log(lengthOfLIS([7, 7, 7, 7, 7, 7, 7])) // 1
-
-// 动态规划 => 返回最长递增子序列
-function lengthOfLIS(nums) {
-  let [len, res, max] = [nums.length, [[0]], 0]
-  for (let i = 1; i < len; i++) {
-    res[i] = [i]
-    for (let j = 0; j < i; j++) {
-      if (nums[j] < nums[i]) {
-        if (res[j].length + 1 > res[i].length) {
-          res[i] = res[j].concat(i)
-        }
-      }
-    }
-    if (res[i].length > res[max].length) max = i
-  }
-  return res[max]
-}
